098078c8a827f007e47301c27112738756edcc1d
Eduardo Rosa <92902577+RosaMaster@users.noreply.github.com>
2023-08-08 19:03:47 -0300

    includ day 24

--- day_22/day_22_pong_game/ball.py
+++ day_22/day_22_pong_game/ball.py
@@ -8,9 +8,9 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
-        self.x_move = 3
-        self.y_move = 3
-        self.move_speed = 0.1
+        self.x_move = 0.5
+        self.y_move = 0.5
+        self.move_speed = 0.2
 
     def move(self):
         new_x = self.xcor() + self.x_move
@@ -26,5 +26,5 @@ class Ball(Turtle):
 
     def reset_position(self):
         self.goto(0, 0)
-        self.move_speed = 0.1
+        self.move_speed = 0.2
         self.bounce_x()
